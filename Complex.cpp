9c1cb5bf14b47b87d27a11611ed2b59d7e40c1de
Andrew Clawson <andrewjclawson18@gmail.com>
2016-04-18 14:29:14 -0600

    Initial Commit

---
+++ Complex.cpp
@@ -0,0 +1,14 @@
+﻿#include "Complex.h"
+
+// Add function definitions here
+Complex::Complex(float re, float im)
+{
+	real = re;
+	imaginary = im;
+}
+
+Complex::Mod()
+{
+	mod = sqrt(pow(real,2) + pow(imaginary,2));
+}
+

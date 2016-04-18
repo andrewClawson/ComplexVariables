5823f20bfaa0aca5a803fbef59adb603393ab985
Andrew Clawson <andrewjclawson18@gmail.com>
2016-04-18 14:56:13 -0600

    Created getters and setters for private members.

--- Complex.cpp
+++ Complex.cpp
@@ -1,14 +1,23 @@
 ﻿#include "Complex.h"
 
+
 // Add function definitions here
+// Constructor function
 Complex::Complex(float re, float im)
 {
 	real = re;
 	imaginary = im;
+	modulus = SetMod(real,imaginary);
+}
+
+//Mod function computes the modulus of the complex number.
+Complex::SetMod(real,imaginary)
+{
+	modulus = sqrt(pow(real,2) + pow(imaginary,2));
 }
 
 Complex::Mod()
 {
-	mod = sqrt(pow(real,2) + pow(imaginary,2));
+	return modulus;
 }
 
--- Complex.h
+++ Complex.h
@@ -1,8 +1,15 @@
-﻿
-#ifndef __MAIN_H__
-#define __MAIN_H__
+﻿class Complex
+{
+	public:
+		Complex(float re,float im);
+		float Re(); // getter for the real component.
+		float Im(); // getter for the imaginary component.
+		void SetMod(); // setter for the modulus.
+		float Mod(); // getter for the modulus.
+	private:
+		float real;
+		float imaginary;
+		float modulus;
+}
 
-// Add function prototypes here
-
-#endif
 
